a362f3fd76657709815144ed6d0957ce6dddd26a
浮云 <1335732406@qq.com>
2022-10-28 09:21:51 +0800

    vertex and index into single buffer

--- header/application.h
+++ header/application.h
@@ -5,35 +5,11 @@
 #include <vector>
 #include <array>
 #include <string>
-#include <glm/glm.hpp>
 
 namespace Clan
 {
-	struct Vertex {
-		glm::vec2 position;
-		glm::vec3 color;
-
-		static VkVertexInputBindingDescription getBindingDescription() {
-			VkVertexInputBindingDescription bindingDescription{};
-			bindingDescription.binding = 0;
-			bindingDescription.stride = sizeof(Vertex);
-			bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-			return bindingDescription;
-		}
-
-		static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
-			std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
-			attributeDescriptions[0].binding = 0;
-			attributeDescriptions[0].location = 0;
-			attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
-			attributeDescriptions[0].offset = offsetof(Vertex, position);
-			attributeDescriptions[1].binding = 0;
-			attributeDescriptions[1].location = 1;
-			attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-			attributeDescriptions[1].offset = offsetof(Vertex, color);
-			return attributeDescriptions;
-		}
-	};
+	struct Vertex;
+	struct UniformBufferObject;
 
 	void DestroyDebugUtilsMessengerEXT(VkInstance instance,
 									   VkDebugUtilsMessengerEXT debugMessenger,
@@ -126,7 +102,7 @@ namespace Clan
 
 		void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
 
-		void createVertexBuffer();
+		void createVertIDBuffer();
 
 		uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
 
@@ -168,7 +144,7 @@ namespace Clan
 		std::vector<VkSemaphore> renderFinishedSemaphores{};
 		std::vector<VkFence> inFlightFences{};
 		uint32_t currentFrame{0};
-		VkBuffer vertexBuffer;
-		VkDeviceMemory vertexBufferMemory;
+		VkBuffer VertIDBuffer;
+		VkDeviceMemory VertIDBufferMemory;
 	};
 }
